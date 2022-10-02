1b12b9881addc33b8a7a62fa39bbfbfa00650794
Shivang Medheakar <69140290+shivangmedhekar@users.noreply.github.com>
2022-10-02 17:27:28 -0400

    Changes in project structure
    files runs user stories from a UserStories/run.py
    made changes in import in main.py
    
    Added function definations comments for US03, 04, 05 and 06 in UserStories

---
+++ UserStories/run.py
@@ -0,0 +1,76 @@
+from user_stories import *
+
+def run_user_stories(individuals, families):
+    """
+    run_user_stories runs all functions in user_stories.py on individuals and famalies in the GEDCOM file
+
+    Args:
+
+        param1 (list): individuals: Is a list of class objects Individual
+        param2 (list): families: Is a list of class objects Family
+    
+    Returns: 
+        (list) logs: It returns list of logs which include erros and successful runs of each user story
+    """ 
+    
+    logs = []
+    for indID in individuals:
+
+        name, sex, birth, death, famsID, famcID = individuals[indID].get_individual()
+
+        # ------------------------- Dates Before Current Date ------------------------ #
+        us01_error = False
+        try:
+            dates_before_current_date(birth, type="Birthday")
+        except Exception as e:
+            us01_error = True
+            logs.append("ERROR: INDIVIDUAL: US01: {}: {}".format(indID,e))
+
+        try:
+            dates_before_current_date(death, type="Deathday")
+        except Exception as e:
+            us01_error = True
+            logs.append("ERROR: INDIVIDUAL: US01: {}: {}".format(indID,e))
+
+        if not us01_error:
+            logs.append("Successful: US01")
+
+        # -------------------------- Less then 150 Years Old ------------------------- #
+        us07_error = False
+        try:
+            less_then_150_years_old(individuals[indID].get_age().years)
+        except (Exception, ValueError) as e:
+            us07_error = True
+            logs.append("ERROR: INDIVIDUAL: US07: {}: {}".format(indID,e))
+
+        if not us07_error:
+            logs.append("Successful: US07")
+
+        # ---------------------------- Birth before death ---------------------------- #
+
+
+        # -------------------------- Marriage before divorce ------------------------- #
+
+
+        # --------------------------- Marriage before death -------------------------- #
+
+
+        # --------------------------- Divorce before death --------------------------- #
+    
+    
+        # -------------------------- Births Before Marriage -------------------------- #
+        us02_error = False
+        fams = individuals[indID].get_famsID()
+
+        for fam in fams:
+            try:
+                marriage_date = families[fam].get_marriage_date()
+                birth_before_marriage(individuals[indID].get_birthday(), marriage_date)
+            except Exception as e:
+                us02_error = True
+                logs.append("ERROR: INDIVIDUAL: US02: {}: {}".format(indID,e))
+
+        if not us02_error:
+            logs.append("Successful: US02")
+
+        return logs
--- UserStories/user_stories.py
+++ UserStories/user_stories.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
+# ---------------------------------------------------------------------------- #
+#                       US 01: Dates before current date                       #
+# ---------------------------------------------------------------------------- #
 def dates_before_current_date(date, type):
 
     if not date and type == "Deathday":
@@ -17,6 +20,9 @@ def dates_before_current_date(date, type):
         raise Exception("{} occurs in the future".format(str(date)))
     
 
+# ---------------------------------------------------------------------------- #
+#                         US 02: Birth before marriage                         #
+# ---------------------------------------------------------------------------- #
 def birth_before_marriage(birthday, marriageDate):
 
     if not marriageDate or not birthday:
@@ -28,6 +34,37 @@ def birth_before_marriage(birthday, marriageDate):
         raise Exception("Birthday {} is after marriage {}".format(str(birthday, str(marriageDate))))
 
 
+# ---------------------------------------------------------------------------- #
+#                           US 03: Birth before death                          #
+# ---------------------------------------------------------------------------- #
+def birth_before_death(birth, death):
+    pass
+
+
+# ---------------------------------------------------------------------------- #
+#                        US 04: Marriage before divorce                        #
+# ---------------------------------------------------------------------------- #
+def marriage_before_divorce(marriage, divorce):
+    pass
+
+
+# ---------------------------------------------------------------------------- #
+#                         US 05: Marriage before death                         #
+# ---------------------------------------------------------------------------- #
+def marriage_before_death(marriage, death):
+    pass
+
+
+# ---------------------------------------------------------------------------- #
+#                          US 06: Divorce before death                         #
+# ---------------------------------------------------------------------------- #
+def divorce_before_death(divorce, death):
+    pass
+
+
+# ---------------------------------------------------------------------------- #
+#                        US 07: Less then 150 years old                        #
+# ---------------------------------------------------------------------------- #
 def less_then_150_years_old(age):
 
     if not age or type(age) is not int:
@@ -39,57 +76,3 @@ def less_then_150_years_old(age):
         return True
     else:
         raise Exception("Greater then 150 years old")
-
-def user_stories(individuals, families):
-    
-    logs = []
-    for indID in individuals:
-
-        name, sex, birth, death, famsID, famcID = individuals[indID].get_individual()
-
-        # ------------------------- Dates Before Current Date ------------------------ #
-        us01_error = False
-        try:
-            dates_before_current_date(birth, type="Birthday")
-        except Exception as e:
-            us01_error = True
-            logs.append("ERROR: INDIVIDUAL: US01: {}: {}".format(indID,e))
-
-        try:
-            dates_before_current_date(death, type="Deathday")
-        except Exception as e:
-            us01_error = True
-            logs.append("ERROR: INDIVIDUAL: US01: {}: {}".format(indID,e))
-
-        if not us01_error:
-            logs.append("Successful: US01")
-
-        # -------------------------- Less then 150 Years Old ------------------------- #
-        us07_error = False
-        try:
-            less_then_150_years_old(individuals[indID].get_age().years)
-        except (Exception, ValueError) as e:
-            us07_error = True
-            logs.append("ERROR: INDIVIDUAL: US07: {}: {}".format(indID,e))
-
-        if not us07_error:
-            logs.append("Successful: US07")
-    
-        # -------------------------- Births Before Marriage -------------------------- #
-        us02_error = False
-        fams = individuals[indID].get_famsID()
-
-        for fam in fams:
-            try:
-                marriage_date = families[fam].get_marriage_date()
-                birth_before_marriage(individuals[indID].get_birthday(), marriage_date)
-            except Exception as e:
-                us02_error = True
-                logs.append("ERROR: INDIVIDUAL: US02: {}: {}".format(indID,e))
-
-        if not us02_error:
-            logs.append("Successful: US02")
-
-        return logs
-
-
