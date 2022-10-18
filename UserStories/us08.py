37528a4f433e721bf344d020fbb7be8603fe0068
Keerthanasharma <89955209+Keerthanasharma@users.noreply.github.com>
2022-10-17 21:58:51 -0400

    Update us08.py

--- UserStories/us08.py
+++ UserStories/us08.py
@@ -16,17 +16,16 @@ def no_children_without_marriage(IDList: List[str], individuals: List[Dict[str, 
 
     for ID in IDList:
 
-        wifeID = families[ID].get_wife()
-        marr = individuals[wifeID].get_marriage_date()
+        marr = families[ID].get_marriage_date()
         marr = datetime(marr).date()
-        divo = individuals[wifeID].get_divorce_date()
+        divo = families[ID].get_divorce_date()
         divo = datetime(divo).date()
         childID = families[ID].get_children()
 
         for child in childID:
             child_birth = individuals[child].get_birthday()
             child_birth = datetime(child_birth).date()
-            
+
             if ((child_birth-marr).days > 30*9):
                 raise Exception(f"Divorce date:{marr} should be 9 months before Child birth date:{child_birth}")
 
