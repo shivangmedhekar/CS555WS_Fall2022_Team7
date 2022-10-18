8a820c03f939eeaf8ae289897297300daada10a9
Keerthanasharma <89955209+Keerthanasharma@users.noreply.github.com>
2022-10-17 20:39:52 -0400

    Update us08.py
    Updated us08.py

--- UserStories/us08.py
+++ UserStories/us08.py
@@ -1,13 +1,14 @@
 from Classes.Individual import Individual
 from Classes.Family import Family
 from typing import List, Dict
+from datetime import datetime
 
 def no_children_without_marriage(IDList: List[str], individuals: List[Dict[str, Individual]], families: List[Dict[str, Family]]) -> bool:
     """
     children should not occur before 9 months since marriage and after 9 months of devorce
     Args:
-        fams (list): list of families the individual is a spouse
-        individuals (dict): dict of individuals {indID: []}
+        IDList(list): a list of families in which the individual is married.
+        individuals (dict): individuals dict indID: []
         families (dict): dict of families {famID: []}
     Returns:
         bool: True if no_children_without_marriage else False
@@ -17,12 +18,15 @@ def no_children_without_marriage(IDList: List[str], individuals: List[Dict[str, 
 
         wifeID = families[ID].get_wife()
         marr = individuals[wifeID].get_marriage_date()
+        marr = datetime(marr).date()
         divo = individuals[wifeID].get_divorce_date()
+        divo = datetime(divo).date()
         childID = families[ID].get_children()
 
         for child in childID:
             child_birth = individuals[child].get_birthday()
-
+            child_birth = datetime(child_birth).date()
+            
             if ((child_birth-marr).days > 30*9):
                 raise Exception(f"Divorce date:{marr} should be 9 months before Child birth date:{child_birth}")
 
